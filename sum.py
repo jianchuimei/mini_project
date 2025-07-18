7204be6eee02ca8c0402825a3eaf5cc53e473c00
jianchuimei <jianchuimeip@gmail.com>
2025-07-18 21:33:12 +0530

    edit forsum of two nimber

--- sum.py
+++ sum.py
@@ -3,3 +3,5 @@ num1 = float(input("enter first number: "))
 num2 = float(input("enter second number: "))
 #calculate the sum of two numbers
 sum = num1 + num2
+#display result
+print("sum of two numbers is: ", sum)
